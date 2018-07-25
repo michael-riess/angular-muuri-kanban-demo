09d49144c15ed2d773ac393c1ff51097df0b147c
Michael <michael.riess@mailfence.com>
2018-07-25 12:40:26 -0400

    basic implementation

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AppRoutingModule } from './modules/app-routing.module';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomePageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
