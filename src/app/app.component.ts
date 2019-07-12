890efc154526e21ff2e0dce4fd58ec4f5b1e795e
Michael Riess <michael.riess@mailfence.com>
2019-07-12 10:38:20 -0400

    feat(): update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    title = 'Muuri Kanban Demo (Angular 6)';
+    title = 'Muuri Kanban Demo (Angular 8)';
 }
