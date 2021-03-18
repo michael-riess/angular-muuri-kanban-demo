5c3ded091f6c5a2b7a8307eea820e70b8b63a6c1
Michael Riess <michael.riess@mailfence.com>
2021-03-18 12:18:32 -0400

    refactor(): eslint changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'Muuri Kanban Demo (Angular 8)';
+    title = 'Muuri Kanban Demo (Angular 11)';
 }
